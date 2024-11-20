c75ab6fb4509b729c5340522fae9488947a53599
Sucith <sucith712@gmail.com>
2024-11-20 22:32:53 +0530

    Added comments into Twosum

--- Easy/Twosum.java
+++ Easy/Twosum.java
@@ -5,11 +5,11 @@ public class Twosum {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int [] arr = new int[5];
-		System.out.println("Enter 5 integers");
+		System.out.println("Enter 5 integers");			// Getting user input
 	
 		
 		for(int i=0;i<arr.length;i++) {
-			arr[i]=in.nextInt();
+			arr[i]=in.nextInt(); 						// printing the array based on the user's input
 		}
 		System.out.println(Arrays.toString(arr));
 		
@@ -19,12 +19,12 @@ public class Twosum {
 		// the main task area
 		
 		boolean found=false;
-		int n = arr.length;
-		for(int a=0;a<arr.length-1;a++) {
-			for(int b=a+1;b<n ; b++) {
-				if(arr[a]+arr[b]==target) {
-					System.out.println(Arrays.toString(new int [] {a,b}));
-					found=true;
+		int n = arr.length;								// n is set to length of the array
+		for(int a=0;a<arr.length-1;a++) {				// a starts from 0 ie from the first value in the array
+			for(int b=a+1;b<n ; b++) {					// b starts next to a
+				if(arr[a]+arr[b]==target) {				// the condition
+					System.out.println(Arrays.toString(new int [] {a,b}));	// printing the array
+					found=true;							// for boolean's sake
 				}
 			}
 		}
